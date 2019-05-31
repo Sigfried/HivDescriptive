3cb34c59de369db43ab23e6b1608ce2a28403644
Sigfried Gold <sigfried.gold@nih.gov>
2019-05-31 10:43:20 -0400

    documented issues with cohort in PackageMaintenance

--- extras/PackageMaintenance.R
+++ extras/PackageMaintenance.R
@@ -21,6 +21,26 @@
 #   HIV patients when HIV onset > age 18
 #   patients with HIV lab test (temporarily use any, ldl measure table record)
 
+# cohorts loading from public ATLAS server:
+
+# cohortId,atlasId,name
+# 1769961,1769961,Male50plus
+# 1769440,1769440,HIV_Patient
+# 1770612,1770612,HIV_patient_by_LOINC_codes
+# 99321,99321,Alendronate
+# 99322,99322,Raloxifene
+# 99323,99323,HipFracture
+# 100792,100792,NoHipVertFx
+# 100791,100791,VertebralFracture
+# 100793,100793,OsteonecrosisOfJaw
+# 100794,100794,EsophagealCancer
+# 100795,100795,AtypicalFF
+# 1769024,1769024,Thromboembolism
+# 1769043,1769043,AcuteStroke%
+
+# things start breaking in CreateCohorts when this one is added:
+# 1770614,1770614,HIV_by_1_SNOMED_Dx
+
 # Insert cohort definitions from ATLAS into package -----------------------
 OhdsiRTools::insertCohortDefinitionSetInPackage(fileName = "CohortsToCreate.csv",
                                                 baseUrl = "http://18.213.176.21:80/WebAPI",
